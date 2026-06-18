0cbc9dc47a3871436c078af1ae9be2c89fa40281
Yazid ELBAK <elbakyazid@gmail.com>
2026-06-18 10:50:36 +0200

    feat: initialiser la structure SentimentAI

---
+++ src/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+from typing import Literal
+
+class PredictionRequest(BaseModel):
+    # Le text à analyser : obligatoire, entre 1 et 5000 caractères
+    text: str = Field(..., min_length=1, max_length=5000)
+
+class PredictionResponse(BaseModel):
+    # Le label retourné est contraint à 3 valeurs possibles
+    label: Literal["POSITIVE", "NEGATIVE", "NEUTRAL"]
+    score: float # Le score de confiance entre 0.0 et 1.0
+    text: str # Texte original retourné pour traceabilité
+    
